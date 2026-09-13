c85b41a401435fb94732f2a095de3a1f0e090ea1
Chetan Satpute <chetan.satpute2002@gmail.com>
2026-09-13 11:13:57 +0530

    feat: add kulala-nvim http client plugin

---
+++ packages/nvim/.config/nvim/lua/plugins/kulala-nvim.lua
@@ -0,0 +1,14 @@
+return {
+  "mistweaverco/kulala.nvim",
+  opts = {
+    treesitter = {
+      enable = false
+    },
+
+    global_keymaps = true,
+    global_keymaps_prefix = "<leader>k",
+
+    kulala_keymaps = true,
+    kulala_keymaps_prefix = "",
+  }
+}
